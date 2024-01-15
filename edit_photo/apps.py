513e907a58c98780da356821c94c87e2886ecb04
sunshinewh <113362169+sunshinewh@users.noreply.github.com>
2024-01-14 18:59:24 -0800

    removeimageapp

--- edit_photo/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EditPhotoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'edit_photo'
--- edit_photo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-	path('edit_photo', views.edit_photo, name='edit_photo'),
-	path('my_photos', views.my_photos, name='my_photos'),
-	path('download/<int:id>', views.download, name='download'),
-]
