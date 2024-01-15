3b29bdba74fc97dc05114548e210a1b32178fcc1
sunshinewh <113362169+sunshinewh@users.noreply.github.com>
2024-01-14 21:45:28 -0800

    disable tasks

--- edit_photo/urls.py
+++ edit_photo/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views
+#from . import views
 
 
 urlpatterns = [
-	path('edit_photo', views.edit_photo, name='edit_photo'),
-	path('my_photos', views.my_photos, name='my_photos'),
-	path('download/<int:id>', views.download, name='download'),
+	#path('edit_photo', views.edit_photo, name='edit_photo'),
+	#path('my_photos', views.my_photos, name='my_photos'),
+	#path('download/<int:id>', views.download, name='download'),
 ]
