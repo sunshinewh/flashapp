a959509cb3557b4679257d5b958c145b39ad4e43
sunshinewh <113362169+sunshinewh@users.noreply.github.com>
2024-01-14 16:06:32 -0800

    fix pymongo via utils.py

--- django_examples/settings.py
+++ django_examples/settings.py
@@ -161,8 +161,8 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-import dejango_heroku
-django_heroku.settings(locals())
+#import dejango_heroku
+#django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
