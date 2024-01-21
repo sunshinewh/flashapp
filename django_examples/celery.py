1948994063a818d5f28f0767cbb4eac9ec09f079
Sunshine Whitton <sunshinebravefireandice.com>
2024-01-21 13:47:29 -0800

    Adding celery components

--- django_examples/celery.py
+++ django_examples/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
 
-app = Celery('your_project')
+app = Celery('django_examples')
 
 # Using Redis as broker
 app.config_from_object('django.conf:settings', namespace='CELERY')
--- flashcards/tasks.py
+++ flashcards/tasks.py
@@ -1,9 +1,9 @@
 # tasks.py in your Django app
 from celery import shared_task
 
-@shared_task
-def generate_image_task(image_params):
+#@shared_task
+#def generate_image_task(image_params):
     # Your image generation logic here
     # ...
     # Return a result, such as an image URL or a status
-    return result
+#    return result
